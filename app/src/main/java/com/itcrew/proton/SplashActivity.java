33236385f70094ea545e8a5fd6093695f0e7e84e
suvasish114 <misuvasish114@gmail.com>
2022-02-14 11:36:43 +0530

    splash activity added

---
+++ app/src/main/java/com/itcrew/proton/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.itcrew.proton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_splash);   // set view method for this class
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class)); // redirect to the main activity using intent
+                finish();   // finisih this activity
+            }
+        }, 5000);   // finish after 1000 millisec
+
+    }
+}
