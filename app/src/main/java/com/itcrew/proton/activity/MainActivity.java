c91b4ae6dba097b84a49bd807f99ae294c5d62e2
suvasish114 <misuvasish114@gmail.com>
2022-02-18 13:52:29 +0530

    runtime location permission added

--- app/src/main/java/com/itcrew/proton/activity/MainActivity.java
+++ app/src/main/java/com/itcrew/proton/activity/MainActivity.java
@@ -1,16 +1,34 @@
 package com.itcrew.proton.activity;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.itcrew.proton.R;
+import com.itcrew.proton.location.Permission;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ask for location permission
+        new Permission().getPermission();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // code
     }
 }
